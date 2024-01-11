ea5b27511e8245f61eea0d3752b5eaf69fe6d4d1
Amir Hosein Javadi <javadiamirhosein.2000@gmail.com>
2024-01-11 13:28:05 -0800

    Add files via upload

--- Code/util/misc.py
+++ Code/util/misc.py
@@ -246,7 +246,7 @@ def init_distributed_mode(args):
                                         world_size=args.world_size, rank=args.rank,
                                         timeout=datetime.timedelta(seconds=54000))
     print("1")
-    torch.distributed.barrier()
+    #torch.distributed.barrier()
     print("2")
     setup_for_distributed(args.rank == 0)
     print("3")
